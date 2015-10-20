c4661f1aeab1fb038680650f29781f943ce96fb9
baiyangx <350916161@qq.com>
2015-10-20 23:12:09 +0800

    1001不含大数 C语言

--- ZOJ1001/main.c
+++ ZOJ1001/main.c
@@ -9,7 +9,10 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // 输入两个数字，输出他们之和（不考虑大数）
+    int A,B;
+    //如何设置了断点的话，在xcode中点击continue programme execution
+    while(scanf("%d %d",&A,&B)!=EOF)//循环输入
+    printf("%d\n",A+B);//换行符。。
     return 0;
 }
